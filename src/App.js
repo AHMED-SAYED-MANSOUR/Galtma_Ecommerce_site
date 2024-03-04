0e98e3d6ee5c805c952710bc2d3f8b01b8c40dd4
AHMED-SAYED-MANSOUR <117373675+AHMED-SAYED-MANSOUR@users.noreply.github.com>
2024-03-04 19:22:29 +0200

    Second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route , Routes } from "react-router-dom";
+import CatHeading from "./components/Categories/Heading/CategoryHeading";
+import NavBar from "./components/NavBar/NavBar";
+import Product from "./components/Product/Product";
+import ADV from "./components/advirtising/AD";
+import Intro from "./components/carousel/Intro";
+import Products from "./components/ProductsList/Products";
+import About from "./components/Pages/About/About";
+import Contact from "./components/Pages/contactus/Contact";
+import Proto from "./components/Pages/Protofolio/proto";
+import ProductPage from "./components/ProductAlone/ProductPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        {/* <NavBar></NavBar> */}
+        <Routes>
+          <Route path="/" element={<><NavBar></NavBar><Intro></Intro> <ADV></ADV> <CatHeading></CatHeading> <Products></Products></>}></Route>
+          <Route path="/About" element={<><NavBar></NavBar><About></About></>}></Route>
+          <Route path="/contactus" element={<><NavBar></NavBar><Contact></Contact></>}></Route>
+          <Route path="/protofolio" element={<><NavBar></NavBar><Proto></Proto></>}></Route>
+          <Route path="/productpage" element={<><ProductPage></ProductPage></>}></Route>
+        </Routes>
+        
+      </div>
     </div>
   );
 }
